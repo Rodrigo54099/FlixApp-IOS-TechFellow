f111b7f74ba1a3905171f3fbbaca17217c6d44f4
Rodrigo54099 <rodrigomartinez1477@gmail.com>
2021-12-03 17:23:46 -0800

    Updated files for Assignment 2

---
+++ Flix App/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Flix App
+//
+//  Created by Rodrigo Martinez on 12/3/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
